bd3dc5f79ac0e995ab559a51280e306fe2269e6b
Rohan <rohan.mpllc@gmail.com>
2018-11-12 22:25:00 +0530

    Updates

---
+++ precache-manifest.1ce3bb8098345b19d243d951bd10c2fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a0e07264ea681102536",
+    "url": "/social/static/js/runtime~main.1a0e0726.js"
+  },
+  {
+    "revision": "e0be1bea5058f5c55311",
+    "url": "/social/static/js/main.e0be1bea.chunk.js"
+  },
+  {
+    "revision": "ed2a8cfae49fb1a1c049",
+    "url": "/social/static/js/1.ed2a8cfa.chunk.js"
+  },
+  {
+    "revision": "e0be1bea5058f5c55311",
+    "url": "/social/static/css/main.ef218207.chunk.css"
+  },
+  {
+    "revision": "ea08cebd670db2d4c414a9a414318ffe",
+    "url": "/social/index.html"
+  }
+];
